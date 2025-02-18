da61f5e45cbed77b5a69329d99839f9fa4834954
Anna M <anna@germ.network>
2025-02-18 10:59:35 -0800

    Pass sub into Login

--- Sources/OAuthenticator/Models.swift
+++ Sources/OAuthenticator/Models.swift
@@ -30,16 +30,24 @@ public struct Token: Codable, Hashable, Sendable {
 public struct Login: Codable, Hashable, Sendable {
 	public var accessToken: Token
 	public var refreshToken: Token?
+	public var did: String?
 
     // User authorized scopes
     public var scopes: String?
 	public var issuingServer: String?
 
-    public init(accessToken: Token, refreshToken: Token? = nil, scopes: String? = nil, issuingServer: String? = nil) {
+	public init(
+		accessToken: Token,
+		refreshToken: Token? = nil,
+		did: String? = nil,
+		scopes: String? = nil,
+		issuingServer: String? = nil
+	) {
 		self.accessToken = accessToken
 		self.refreshToken = refreshToken
 		self.scopes = scopes
 		self.issuingServer = issuingServer
+		self.did = did
 	}
 
 	public init(token: String, validUntilDate: Date? = nil) {
