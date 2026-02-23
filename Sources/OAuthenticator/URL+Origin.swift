488435dcbe08c5b9415434ce7af54307f908e602
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2026-02-23 22:30:06 +0100

    Simplify URL+Origin

--- Sources/OAuthenticator/URL+Origin.swift
+++ Sources/OAuthenticator/URL+Origin.swift
@@ -4,38 +4,30 @@ import Foundation
 	import FoundationNetworking
 #endif
 
-public enum URLError: Error, Hashable {
-	case relative(String)
-}
-
-let insecureSchemes = ["http", "ws"]
-let secureSchemes = ["https", "wss"]
-
 extension URL {
 	var origin: String? {
 		guard
 			let host = self.host,
 			let scheme = self.scheme
 		else {
-			// throw URLError.relative("Cannot calculate an origin for a relative URL")
 			return nil
 		}
-		
+
 		var originComponents = URLComponents()
 		originComponents.scheme = scheme
 		originComponents.host = host
-		
+
 		guard let port = self.port else {
 			return originComponents.string
 		}
 
-		let isStandardPort =
-			(insecureSchemes.contains(scheme) && port == 80)
-			|| (secureSchemes.contains(scheme) && port == 443)
-
-		let isHttp = insecureSchemes.contains(scheme) || secureSchemes.contains(scheme)
-
-		if (isHttp && !isStandardPort) || !isHttp {
+		if scheme == "http" || scheme == "https" {
+			if scheme == "http" && port != 80 {
+				originComponents.port = port
+			} else if scheme == "https" && port != 443 {
+				originComponents.port = port
+			}
+		} else {
 			originComponents.port = port
 		}
 
